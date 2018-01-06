453636b95eb41850a3e2aa9f947a1a115a13248b
Newbie <linhdqtg@gmail.com>
2018-01-06 10:22:42 +0700

    Update defaults.js
    Info update

--- config/defaults.js
+++ config/defaults.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: "pool.supportxmr.com",
+  host: "etn-pool.semipool.com",
   port: 3333,
   pass: "x",
   ssl: false,
-  address: null,
+  address: "etnkMVskQTfNtXBf56EZLzg8FhPf7iko9gukXstYX6uYAMHcyTVjkVCfDYkMYBG4T84zYdu8P6BhwEWz2Ab1SDBh7AcmgiPTNf",
   user: null,
   diff: null,
   dynamicPool: false,
@@ -11,8 +11,8 @@ module.exports = {
   donations: [
     {
       address: "46WNbmwXpYxiBpkbHjAgjC65cyzAxtaaBQjcGpAZquhBKw2r8NtPQniEgMJcwFMCZzSBrEJtmPsTR54MoGBDbjTi2W1XmgM",
-      host: "pool.supportxmr.com",
-      port: 3333,
+      host: "gulf.moneroocean.stream",
+      port: 10001,
       user: null,
       pass: "donations",
       percentage: 0.01 // 1%
